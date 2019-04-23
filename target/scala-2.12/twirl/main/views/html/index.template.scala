5fbf74787ce859c56e423a648ace3042b628bec1
Colm Dunne <x00136264@gmail.com>
2019-04-23 15:23:04 +0100

    main

--- target/scala-2.12/twirl/main/views/html/index.template.scala
+++ target/scala-2.12/twirl/main/views/html/index.template.scala
@@ -48,46 +48,53 @@ Seq[Any](format.raw/*1.27*/("""
 
 
 	<div class="w3-container">
-		<h2>New shoes available</h2>
-		<p>These shoes are a limited time offer! </p>
+		<h2>New items available</h2>
+		<p>These items are available while stocks last! </p>
 	  </div>
 	  
 	  <div class="w3-content w3-display-container">
 	  
 	  <div class="w3-display-container mySlides">
-		<img src="https://www.technobuffalo.com/sites/technobuffalo.com/files/styles/large/public/wp/2019/01/1nike-adapt-bb.jpg" style="width:80%">
+		<img src="/assets/images/productImages/top1.jpg" style="width:80%">
 		<div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
-		Nike's new Adapt BB &euro; 150
+		Nike Athlete T-Shirt &euro; 30
 		</div>
 	  </div>
 	  
 	  <div class="w3-display-container mySlides">
-		<img src="https://cdn-images.farfetch-contents.com/13/19/70/13/13197013_14715715_1000.jpg" style="width:40%">
+		<img src="/assets/images/productImages/shorts1.jpg" style="width:90%">
 		<div class="w3-display-bottomright w3-large w3-container w3-padding-16 w3-black">
-			BALENCIAGA Speed knitted sneakers  &euro; 400
+			Under Armour Performance Shorts  &euro; 25
 		</div>
 	  </div>
 	  
 	  <div class="w3-display-container mySlides">
-		<img src="https://static.mainlinemenswear.co.uk/shopimages/products/normal/0000%20a%20%20%20827%20007.jpg" style="width:55%">
+		<img src="/assets/images/productImages/leggings1.jpg" style="width:90%">
 		<div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">
-			BOSS Athleisure Spacit Trainers Black  &euro; 80
+			Nike Womens Leggings  &euro; 45
 		</div>
 	  </div>
 	  
 	  <div class="w3-display-container mySlides">
-		<img src="https://tpc.googlesyndication.com/simgad/14045841722005816001" style="width:100%">
+		<img src="/assets/images/productImages/hoodie1.jpg" style="width:100%">
 		<div class="w3-display-topright w3-large w3-container w3-padding-16 w3-black">
-		 Fila Runners Women  &euro; 100
+		 Adidas Sport Hoodie  &euro; 50
 		</div>
 	  </div>
 	  
 	  <div class="w3-display-container mySlides">
-		<img src="https://media.gucci.com/style/DarkGray_Center_0_0_800x800/1530812705/536532_GGZ30_1261_001_100_0000_Light-Flashtrek-sneaker-with-removable-crystals.jpg" style="width:55%">
+		<img src="/assets/images/productImages/runners1.jpg" style="width:80%">
 		<div class="w3-display-middle w3-large w3-container w3-padding-16 w3-black">
-			Gucci Flashtrek sneaker with removable crystals  &euro; 1000
+			Nike Air Max 270s  &euro; 150
 		</div>
-	  </div>
+		</div>
+		
+		<div class="w3-display-container mySlides">
+			<img src="/assets/images/productImages/jacket1.jpg" style="width:90%">
+			<div class="w3-display-middle w3-large w3-container w3-padding-16 w3-black">
+				Under Armour Windstopper Jacket  &euro; 185
+			</div>
+			</div>
 	  
 	  <button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>
 	  <button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">&#10095;</button>
@@ -98,21 +105,21 @@ Seq[Any](format.raw/*1.27*/("""
 	  var slideIndex = 1;
 	  showDivs(slideIndex);
 	  
-	  function plusDivs(n) """),format.raw/*69.25*/("""{"""),format.raw/*69.26*/("""
-		"""),format.raw/*70.3*/("""showDivs(slideIndex += n);
-	  """),format.raw/*71.4*/("""}"""),format.raw/*71.5*/("""
+	  function plusDivs(n) """),format.raw/*76.25*/("""{"""),format.raw/*76.26*/("""
+		"""),format.raw/*77.3*/("""showDivs(slideIndex += n);
+	  """),format.raw/*78.4*/("""}"""),format.raw/*78.5*/("""
 	  
-	  """),format.raw/*73.4*/("""function showDivs(n) """),format.raw/*73.25*/("""{"""),format.raw/*73.26*/("""
-		"""),format.raw/*74.3*/("""var i;
+	  """),format.raw/*80.4*/("""function showDivs(n) """),format.raw/*80.25*/("""{"""),format.raw/*80.26*/("""
+		"""),format.raw/*81.3*/("""var i;
 		var x = document.getElementsByClassName("mySlides");
-		if (n > x.length) """),format.raw/*76.21*/("""{"""),format.raw/*76.22*/("""slideIndex = 1"""),format.raw/*76.36*/("""}"""),format.raw/*76.37*/("""
-		"""),format.raw/*77.3*/("""if (n < 1) """),format.raw/*77.14*/("""{"""),format.raw/*77.15*/("""slideIndex = x.length"""),format.raw/*77.36*/("""}"""),format.raw/*77.37*/("""
-		"""),format.raw/*78.3*/("""for (i = 0; i < x.length; i++) """),format.raw/*78.34*/("""{"""),format.raw/*78.35*/("""
-		   """),format.raw/*79.6*/("""x[i].style.display = "none";  
-		"""),format.raw/*80.3*/("""}"""),format.raw/*80.4*/("""
-		"""),format.raw/*81.3*/("""x[slideIndex-1].style.display = "block";  
-	  """),format.raw/*82.4*/("""}"""),format.raw/*82.5*/("""
-	  """),format.raw/*83.4*/("""</script>
+		if (n > x.length) """),format.raw/*83.21*/("""{"""),format.raw/*83.22*/("""slideIndex = 1"""),format.raw/*83.36*/("""}"""),format.raw/*83.37*/("""
+		"""),format.raw/*84.3*/("""if (n < 1) """),format.raw/*84.14*/("""{"""),format.raw/*84.15*/("""slideIndex = x.length"""),format.raw/*84.36*/("""}"""),format.raw/*84.37*/("""
+		"""),format.raw/*85.3*/("""for (i = 0; i < x.length; i++) """),format.raw/*85.34*/("""{"""),format.raw/*85.35*/("""
+		   """),format.raw/*86.6*/("""x[i].style.display = "none";  
+		"""),format.raw/*87.3*/("""}"""),format.raw/*87.4*/("""
+		"""),format.raw/*88.3*/("""x[slideIndex-1].style.display = "block";  
+	  """),format.raw/*89.4*/("""}"""),format.raw/*89.5*/("""
+	  """),format.raw/*90.4*/("""</script>
 
 </div>
 </div>
@@ -129,7 +136,7 @@ Seq[Any](format.raw/*1.27*/("""
 </div>
 <div class="col-sm-8"></div>
 </div>
-""")))}),format.raw/*100.2*/("""
+""")))}),format.raw/*107.2*/("""
 
 """))
       }
@@ -147,11 +154,11 @@ Seq[Any](format.raw/*1.27*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Thu Apr 18 14:39:12 IST 2019
+                  DATE: Tue Apr 23 14:20:55 IST 2019
                   SOURCE: /home/wdd/ProjectYear2/app/views/index.scala.html
-                  HASH: c796031838b0a4e1f0e7340ae5ac55957c81aa0b
-                  MATRIX: 959->1|1079->26|1107->29|1133->47|1171->48|1200->51|3773->2596|3802->2597|3833->2601|3891->2632|3919->2633|3956->2643|4005->2664|4034->2665|4065->2669|4177->2753|4206->2754|4248->2768|4277->2769|4308->2773|4347->2784|4376->2785|4425->2806|4454->2807|4485->2811|4544->2842|4573->2843|4607->2850|4668->2884|4696->2885|4727->2889|4801->2936|4829->2937|4861->2942|5185->3235
-                  LINES: 28->1|33->1|34->2|34->2|34->2|35->3|101->69|101->69|102->70|103->71|103->71|105->73|105->73|105->73|106->74|108->76|108->76|108->76|108->76|109->77|109->77|109->77|109->77|109->77|110->78|110->78|110->78|111->79|112->80|112->80|113->81|114->82|114->82|115->83|132->100
+                  HASH: 4462d76c158c4315df9da92b6884f1eb2d1827be
+                  MATRIX: 959->1|1079->26|1107->29|1133->47|1171->48|1200->51|3711->2534|3740->2535|3771->2539|3829->2570|3857->2571|3894->2581|3943->2602|3972->2603|4003->2607|4115->2691|4144->2692|4186->2706|4215->2707|4246->2711|4285->2722|4314->2723|4363->2744|4392->2745|4423->2749|4482->2780|4511->2781|4545->2788|4606->2822|4634->2823|4665->2827|4739->2874|4767->2875|4799->2880|5123->3173
+                  LINES: 28->1|33->1|34->2|34->2|34->2|35->3|108->76|108->76|109->77|110->78|110->78|112->80|112->80|112->80|113->81|115->83|115->83|115->83|115->83|116->84|116->84|116->84|116->84|116->84|117->85|117->85|117->85|118->86|119->87|119->87|120->88|121->89|121->89|122->90|139->107
                   -- GENERATED --
               */
           
